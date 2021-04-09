e4e189e852385b6da75e3d8c738fba3069f9f6a3
ashot_hambardzumyan <ashot_hambardzumyan@inbox.ru>
2021-04-10 01:15:25 +0400

    Add project files.

---
+++ ConsoleApp11/IMovable.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp11
+{
+    interface IMovable
+    {
+        public const int minSpeed = 0;
+        private static int maxSpeed = 60;
+
+        static double GetTime(double distance, double speed) => distance / speed;
+        static int MaxSpeed
+        {
+            get { return maxSpeed; }
+            set
+            {
+                if (value > 0) maxSpeed = value;
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BadWeather badWeather = new BadWeather();
+            GoodWeather goodWeather = new GoodWeather();
+
+            IRainable rainable = badWeather as IRainable;
+            rainable.BadMethod();
+            IWindable windable = badWeather as IWindable;
+            windable.BadMethod();
+
+            ISunable sunable = goodWeather as ISunable;
+            sunable.GoodMethod();
+            IWarmable warmable = goodWeather as IWarmable;
+            warmable.GoodMethod();
+
+            Console.ReadKey();
+        }
+    }
+    interface IRainable
+    {
+        void BadMethod();
+    }
+    interface IWindable : IRainable
+    {
+        new void BadMethod();
+    }
+
+    class BadWeather : IWindable
+    {
+        void IRainable.BadMethod()
+        {
+            Console.WriteLine("Is Raining: ");
+        }
+        void IWindable.BadMethod()
+        {
+            Console.WriteLine("Is Windy: ");
+        }
+    }
+
+    interface ISunable
+    {
+        void GoodMethod();
+    }
+    interface IWarmable : ISunable
+    {
+        new void GoodMethod();
+    }
+    
+    class GoodWeather : IWarmable
+    {
+        void ISunable.GoodMethod()
+        {
+            Console.WriteLine("Is Sunny: ");
+        }
+        void IWarmable.GoodMethod()
+        {
+            Console.WriteLine("Is Warming: ");
+        }
+    }
+}
